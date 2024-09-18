0ca463ca0a9d20a0679f01f874a00735ad51e560
gablook6 <gabriel.look.1@ens.etsmtl.ca>
2024-09-18 13:33:16 -0400

    Test Commit 2

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,9 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-    <a href="https://google.ca/" target="_blank"></a>
+    <a href="https://google.ca/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
